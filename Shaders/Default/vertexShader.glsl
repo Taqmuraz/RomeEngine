a7b090f65f349d5411909936b6456ea28a5ef408
taimuraz.fardzinov <Taimuraz.Fardzinov@aspose.com>
2022-09-23 15:52:05 +0200

    arial font

--- Shaders/Default/vertexShader.glsl
+++ Shaders/Default/vertexShader.glsl
@@ -15,14 +15,10 @@ void main ()
 {
 	pass_textureCoords = textureCoords;
 	
-	mat4 vt = viewMatrix * transformationMatrix;
-	vec4 world = vt * vec4(position, 1.0);
-	vec4 pos = projectionMatrix * world;
-	
-	vec4 surfaceNormal4 = vt * vec4(normal.x, normal.y, normal.z, 0.0);
+	vec4 surfaceNormal4 = transformationMatrix * vec4(normal.x, normal.y, normal.z, 0.0);
 	surfaceNormal = normalize(vec3(surfaceNormal4.x, surfaceNormal4.y, surfaceNormal4.z));
 	
-	gl_Position = pos;
+	gl_Position = projectionMatrix * viewMatrix * transformationMatrix * vec4(position, 1);
 }
 
 
--- Shaders/Text/vertexShader.glsl
+++ Shaders/Text/vertexShader.glsl
@@ -6,11 +6,12 @@ in vec2 textureCoords;
 out vec2 uv;
 
 uniform vec4 vertexRect;
+uniform float depth;
 
 void main ()
 {
 	uv = textureCoords;
-	gl_Position = vec4(vertexRect.x + position.x * vertexRect.z, vertexRect.y + position.y * vertexRect.w, -0.5, 1.0);
+	gl_Position = vec4(vertexRect.x + position.x * vertexRect.z, vertexRect.y + position.y * vertexRect.w, depth, 1.0);
 }
 
 
